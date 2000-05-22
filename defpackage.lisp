95e0d8b68ec63516546853ace43ddeabab8bf1e9
dan <none>
2000-05-22 23:54:38 +0000

    wouldn't it be cool to have working EQUAL hash tables?
    darcs-hash:48ddb64712f377c2569e77beffec328ecd9a3731

--- defpackage.lisp
+++ defpackage.lisp
@@ -2,3 +2,8 @@
 (defpackage "CLIKI"
   (:export cliki-view-handler cliki-edit-handler)
   (:use "MAKE" "COMMON-LISP" "ARANEIDA"))
+
+;;; used for interning page titles in, and not a lot else.  Working
+;;; EQUAL hash table would be nice.
+(defpackage "CLIKI-PAGES")
+
