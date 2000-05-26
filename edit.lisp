dfaf9206355af80b8cd0541013dc62519cbc4933
dan <none>
2000-05-26 23:27:58 +0000

    the entire system is now composed of functions VIEW-PAGE, EDIT-PAGE
    the entire system is now composed of functions VIEW-PAGE, EDIT-PAGE
    and SAVE-PAGE.  This file holds the latter two.
    
    darcs-hash:cb7cdd689ee0f8789cacf19935458ff6a8e9e182

--- edit.lisp
+++ edit.lisp
@@ -1,33 +1,43 @@
 (in-package :cliki)
 
-;;; copy-stream isn't good enough: we have to do something clever with
-;;; bits of HTML (like, form elements) embedded in Lisp code, otherwise
-;;; we leave the form forthwith
+;;; need to put in check for simultaneous edits
 
-;;; need to put in check for simultaneous edits, too.
-
-
-(defun cliki-edit-handler (request arg-string root)
-  (let* ((file (merge-pathnames arg-string root))
-         (out (request-stream request))
-         (view-href (format nil "<a href=\"../~A\">~A</a>"
-                            arg-string arg-string))
-         (method (request-method request)))
-    (case method
-      ((:head :get)
-       (request-send-headers request)
-       (format (request-stream request)
-               "<html><head><title>Cliki : Edit ~A</title></head>
-<body><h1>Edit ~A</h1>
+(defun edit-page (request title root)
+  (let* ((out (request-stream request))
+         (view-href (format nil "<a href=\"~A\">~A</a>"
+                            (request-path-info request) title)))
+    (request-send-headers request :expires (get-universal-time))
+    (format (request-stream request)
+            "<html><head><title>Cliki : Edit ``~A''</title></head>
+<body><h1>Edit ``~A''</h1>
 <form method=post>
-<textarea name=text rows=10 cols=60>" arg-string view-href arg-string)
-       (if (probe-file file)
-           (with-open-file (in file :direction :input)
-             (if (ignore-errors (peek-char nil in nil))
-                 (araneida::copy-stream in out))))
-       (format out "</textarea><br><input type=submit value=Save name=Save></form></body></html>"))
-      ((:post)
-       (with-open-file (out-file file :direction :output)
-         (write-sequence (body-param "text" (request-body request)) out-file))
-       (request-send-headers request)
-       (format out "Thanks for editing ~A" view-href)))))
+<textarea wrap=virtual name=text rows=20 cols=80>~%"
+            title view-href)
+    (handler-case
+     (with-open-file (in (merge-pathnames title root) :direction :input)
+       ;; XXX copy-stream isn't good enough: we have to do something
+       ;; clever with bits of HTML (like, form elements) embedded in
+       ;; Lisp code, otherwise we leave the form forthwith
+       (araneida::copy-stream in out))
+     (file-error (e) ;; probably it just doesn't exist: not actually an error
+                 (declare (ignore e))
+                 (format out "Describe _(~A) here~%" title ))
+     (error (e) (format out "Describe _(~A) here~%~A~%" title e)))
+    (format out "</textarea><br><input type=submit value=Save name=Save></form></body></html>")))
+
+(defun save-page (request title root)
+  (let* ((file (merge-pathnames title root))
+         (out (request-stream request))
+         (view-href (format nil "<a href=\"~A\">~A</a>"
+                            (request-path-info request) title)))
+    (handler-case 
+     (with-open-file (out-file file :direction :output)
+       (write-sequence (body-param "text" (request-body request)) out-file))
+     (error (c) (request-send-error 500 "Unable to save file: ~A" c))
+     (:no-error (c)
+                (declare (ignorable c))
+                (request-send-headers request)
+                (format out "Thanks for editing ~A.  You probably need to `reload' or `refresh' to see your changes take effect" view-href)))))
+
+
+
