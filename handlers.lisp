3e502125982d77d559010a9d1af2c4b84739ca1e
dan <none>
2000-05-27 01:43:10 +0000

    (request-title request) returns the title of the page being requested
    (request-title request) returns the title of the page being requested
    Among other useful tasks it strips out ////, so /cliki///////etc/passwd
    doesn't work any more
    
    cliki-list-all-pages-handler does as its name suggests
    
    darcs-hash:ac7c369e4d37a52b184316bad7798233dbdd4e0d

--- handlers.lisp
+++ handlers.lisp
@@ -1,8 +1,14 @@
 (in-package :cliki)
 
+(defun request-title (request)
+  (let* ((string (request-path-info request))
+         (pos (position #\/ string :from-end t)))
+    (urlstring-unescape
+     (subseq string (if pos (1+ pos) 0)))))
+
 (defun cliki-get-handler (request arg-string root)
   (let* ((action (url-query (request-url request)))
-         (title (urlstring-unescape (request-path-info request)))
+         (title (request-title request))
          (file (merge-pathnames title root))
          (out (request-stream request)))
     (cond
@@ -15,8 +21,27 @@
       (request-send-error request 500 "Eh?")))))
 
 (defun cliki-post-handler (request arg-string root)
-  (let ((title (urlstring-unescape (request-path-info request))))
+  (let ((title  (request-title request)))
     (save-page request title root)))
+
+(defun cliki-list-all-pages-handler (request arg-string url-root fs-root)
+  (let ((pages (mapcar #'pathname-name (directory fs-root :sort t))))
+    (request-send-headers request)
+    (write-sequence
+     (html
+      `(html
+        (head (title "Cliki: All pages"))
+        (body
+         (h1 "All pages")
+         (ul
+          ,@(mapcar
+             (lambda (x)
+               `(li ((a :href
+                        ,(urlstring (merge-url url-root (urlstring-escape x))))
+                     ,x)))
+                pages)))))
+     (request-stream request))))
+
 
 (defun export-handlers (base-url directory)
   (export-handler base-url (list 'cliki-get-handler directory)
@@ -25,5 +50,7 @@
                   :method :head)
   (export-handler base-url (list 'cliki-post-handler directory)
                   :method :post)
+  (export-handler (merge-url base-url "admin/all-pages")
+                  (list 'cliki-list-all-pages-handler base-url directory))
   (export-handler base-url (lambda (r rest) (request-redirect r "index"))
                   :method :get :match :exact))
