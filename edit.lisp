8ebe7d0fca121320a6c1a0e9958ae2931148b5ad
dan <none>
2001-05-14 13:47:53 +0000

    added more boxes to page editing form, for change summary and author
    added more boxes to page editing form, for change summary and author
    -(defun update-recent-changes (title root)
    - deleted whole function
    
    darcs-hash:b305cfcaf81f8b4a303c07f07d1f7bf173b26f10

--- edit.lisp
+++ edit.lisp
@@ -24,60 +24,28 @@
                  (declare (ignore e))
                  (format out "Describe _(~A) here~%" title ))
      (error (e) (format out "Describe _(~A) here~%~A~%" title e)))
-    (format out "</textarea><br><input type=submit value=Save name=Save></form></body></html>")))
+    (format out "</textarea>
+<br><b>Summary of changes: <input type=text size=60 name=summary value=\"?\">
+<br><b>Your name: <input type=text size=30 name=name value=\"A N Other\">
+<br><input type=submit value=Save name=Save></form></body></html>")))
 
 (defun save-page (request title root)
   (let* ((file (merge-pathnames title root))
          ;;(ndx-file (merge-pathnames (make-pathname :type "ndx") file))
          (out (request-stream request))
+         (body (request-body request))
          (view-href (format nil "<a href=\"~A\">~A</a>"
                             (request-path-info request) title)))
     (handler-case 
      (with-open-file (out-file file :direction :output)
-       (write-sequence (body-param "text" (request-body request)) out-file))
-     ;;(with-open-file (out-ndx ndx-file :direction :output)
-     ;; (prin1 (index-for (body-param "text" (request-body request))) out-ndx))
+       (write-sequence (body-param "text" body) out-file))
      (error (c) (request-send-error 500 "Unable to save file: ~A" c))
      (:no-error (c)
                 (declare (ignorable c))
+                (add-recent-change root (get-universal-time) title
+                                   (body-param "name" body)
+                                   (body-param "summary" body))
                 (request-send-headers request)
-                (format out "Thanks for editing ~A.  Don't forget to update the <a href=\"Recent+Changes\">Recent Changes</a> page to describe what you did.  Oh, and you probably need to `reload' or `refresh' to see your changes take effect" view-href)))
+                (format out "Thanks for editing ~A.  You probably need to `reload' or `refresh' to see your changes take effect" view-href)))
     (update-indexes-for-page title root)))
-
-;;; This is fairly intolerant about the format of the file it reads.
-(defun update-recent-changes (title root)
-  (let* ((pathname (merge-pathnames "Recent Changes" root))
-         (now-decoded (multiple-value-list (get-decoded-time)))
-         (this-morning (encode-universal-time 0 0 0
-                                              (elt now-decoded 3)
-                                              (elt now-decoded 4)
-                                              (elt now-decoded 5)
-                                              (elt now-decoded 6)))
-         (document
-          (with-open-file (in pathname :direction :input)
-            (copy-tree (net.html.parser:parse-html in))))
-         (last-change (ext:parse-time
-                       (second
-                        (find-if (lambda (x) (and (consp x) (eql (car x) :h3)))
-                                 document)))))
-    (unless (>= last-change this-morning)
-      (setf document
-            `(,(car document)
-              (:h3 ,(with-date this-morning
-                      (format nil "~/cliki:dayname/, ~A ~/cliki:monthname/ ~A"
-                              day date month year)))
-              #.(format nil "~%")
-              (:ul)
-              #.(format nil "~%")
-              ,@(cdr document))))
-    (let ((ul (find-if (lambda (x) (and (consp x) (eql (car x) :ul))) document)))
-      (setf (cdr ul)
-            (cons (list :li (format nil "_(~A): something changed" title))
-                  (cdr ul))))
-    
-    (with-open-file (out pathname :direction :output)
-      (dolist (tree document)
-        (araneida::html-stream out tree net.html.parser::*in-line*)))))
-
-                           
 
