776023c8580a9a05e7b4507683f28a6f516431ea
dan <none>
2001-05-14 11:31:02 +0000

    -  (:use "MAKE" "COMMON-LISP" "ARANEIDA"))
     -  (:use "MAKE" "COMMON-LISP" "ARANEIDA"))
     +  (:use "MAKE" "COMMON-LISP" "ARANEIDA" "SOCKETS"))
    
    darcs-hash:c03005ff739656bca37dbf49063f087879aea66f

--- defpackage.lisp
+++ defpackage.lisp
@@ -1,7 +1,7 @@
 (in-package :user)
 (defpackage "CLIKI"
   (:export cliki-view-handler cliki-edit-handler)
-  (:use "MAKE" "COMMON-LISP" "ARANEIDA"))
+  (:use "MAKE" "COMMON-LISP" "ARANEIDA" "SOCKETS"))
 
 ;;; language for inline searches and stuff
 (defpackage "CLIKI-PAGES" (:use #| nil |# ))
