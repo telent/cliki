74cf9852f337cb4e85fae1fad0625880bd4d2a53
dan <none>
2001-07-23 00:00:39 +0000

    add (defun cliki-search-handler (request rest-of-url)
    add (defun cliki-search-handler (request rest-of-url)
    add overall cliki-handler which change-classes then redispatches on
    our internal table
    (defun test () ) - easier than setting up telentweb all the time.
    This is _not_ production code
    
    darcs-hash:6939dc6c60333d2b125be48e41f2e4ad1082ea78

--- handlers.lisp
+++ handlers.lisp
@@ -1,5 +1,13 @@
 (in-package :cliki)
 
+(defclass cliki-request (request)
+  ((url-root :accessor cliki-request-url-root :initarg :url-root)
+   (data-directory :accessor cliki-request-data-directory
+		   :initarg :data-directory)))
+   
+(defvar *cliki-handlers* (list nil))
+(defvar *cliki-initialized-p* nil)
+
 (defun find-page-name (title root)
   (labels ((match-p (a b)
                     (string-equal (substitute #\_ #\Space a)
@@ -19,12 +27,11 @@
                       (merge-url (request-url request) (urlstring-escape actual)))
     nil))
 
-(defun cliki-get-handler (request arg-string root)
-  (let ((title (request-title request root)))
+(defun cliki-get-handler (request arg-string)
+  (let* ((root (cliki-request-data-directory request))
+	 (title (request-title request root)))
     (unless title (return-from cliki-get-handler))
-    (let ((action (url-query (request-url request)))
-          (file (merge-pathnames title root))
-          (out (request-stream request)))
+    (let ((action (url-query (request-url request))))
       (cond
        ((not action)
         (view-page request title root))
@@ -36,8 +43,9 @@
        (t
         (request-send-error request 500 "Eh?"))))))
 
-(defun cliki-head-handler (request arg-string root)
-  (let ((title (request-title request root)))
+(defun cliki-head-handler (request arg-string)
+  (let* ((root (cliki-request-data-directory request))
+	 (title (request-title request root)))
     (unless title (return-from cliki-head-handler))
     (let* ((file (merge-pathnames title root))
            (date (file-write-date file)))
@@ -46,12 +54,15 @@
         (request-send-headers request :response-code 404
                               :response-text "Not found")))))
 
-(defun cliki-post-handler (request arg-string root)
-  (let ((title  (request-title request root)))
+(defun cliki-post-handler (request arg-string)
+  (let* ((root (cliki-request-data-directory request))
+	 (title  (request-title request root)))
     (if title (save-page request title root))))
 
-(defun cliki-list-all-pages-handler (request arg-string url-root fs-root)
-  (let ((pages (mapcar #'pathname-name (directory fs-root :sort t))))
+(defun cliki-list-all-pages-handler (request arg-string)
+  (let* ((fs-root (cliki-request-data-directory request))
+	 (url-root (cliki-request-url-root request))
+	 (pages (mapcar #'pathname-name (directory fs-root :sort t))))
     (request-send-headers request)
     (write-sequence
      (html
@@ -68,22 +79,87 @@
                 pages)))))
      (request-stream request))))
 
+(defun cliki-search-handler (request rest-of-url)
+  (let* ((url (request-url request))
+	 (words (car (url-query-param url  "words")))
+	 (results (search-for-string (urlstring-unescape words)))
+	 (start (parse-integer
+		 (or (car (url-query-param url "start"))
+		     "0") :junk-allowed t))
+	 (end (min (length results) (+ start 10)))
+	 (out (request-stream request)))
+    (request-send-headers request)
+    (send-cliki-page-preamble request "Search results")
 
-(defun export-handlers (base-url directory)
+    (format out "<form action=\"~Aadmin/search\"> Search again: <input name=words size=20 value=~S></form>"
+	    (urlstring (cliki-request-url-root request)) words)
+    (cond (results
+	   (format out "<table>")
+	   (loop for (name . rel) in (subseq results start end)
+		 for j from start to end
+		 do (format out "~&<tr><td>~A</td><td><a href=\"../~A\">~A</a> (~,01F% relevant)</td></tr>"
+			    (1+ j)
+			    (urlstring-escape (pathname-name (parse-namestring name)))
+			    (pathname-name (parse-namestring name))
+			    (* rel 100)))
+	   (format out "~&</table>")
+	   (print-page-selector
+	    (request-stream request) start 10 (length results)
+	    (format nil "~A?words=~A&start="
+		    (url-path url)
+		    (urlstring-escape words))))
+	  (t (format out "Sorry, no pages match your search term.  Whack fol o diddle i ay")))))
+		     
+
+#|
+(setf *cliki-initialized-p* nil)
+|#
+(defun cliki-handler (request exports discriminator arg-string fs-root)
+  (declare (ignore exports arg-string))
+  (unless *cliki-initialized-p*
+    (initialize (request-base-url request) fs-root))
+  (change-class request 'cliki-request)
+  (setf (cliki-request-url-root request) (request-base-url request)
+	(cliki-request-data-directory request) fs-root)
+  (dispatch-request request *cliki-handlers* discriminator))
+
+;;; these are bound in cliki-handler
+
+(defun initialize (base-url directory)
+  (setf *last-directory-time* 0)
   (create-indexes directory)
+  (setf  *cliki-handlers* (list nil))
+  (setf *tfidf-index* (reindex-text directory))
   (restore-recent-changes directory)
-  (export-handler base-url (list 'cliki-get-handler directory)
-                  :method :get)
-  (export-handler base-url (list 'cliki-head-handler directory)
-                  :method :head)
-  (export-handler base-url (list 'cliki-post-handler directory)
-                  :method :post)
+  (export-handler base-url 'cliki-get-handler
+		  :method :get :stage *cliki-handlers*)
+  (export-handler base-url 'cliki-head-handler
+		  :method :head :stage *cliki-handlers*)
+  (export-handler base-url 'cliki-post-handler 
+		  :method :post :stage *cliki-handlers*)
   (export-handler (merge-url base-url "admin/all-pages")
-                  (list 'cliki-list-all-pages-handler base-url directory))
+                  'cliki-list-all-pages-handler :stage *cliki-handlers*)
+  (export-handler (merge-url base-url "admin/cliki.css")
+		  'css-file-handler  :stage *cliki-handlers*)
+  (export-handler (merge-url base-url "admin/search")
+		  'cliki-search-handler :stage *cliki-handlers*)
   (export-handler (merge-url base-url "Recent%20Changes")
-                  (list 'view-recent-changes directory))
+		  `(view-recent-changes) :stage *cliki-handlers*)
   (export-handler (merge-url base-url "Recent+Changes")
-                  (list 'view-recent-changes directory))
-  (export-handler base-url (lambda (r rest) (request-redirect r "index"))
-                  :method :get :match :exact))
+		  `(view-recent-changes) :stage *cliki-handlers*)
+  (setf *cliki-initialized-p* t))
+
+
+(defun test ()
+  (let ((base-url (parse-urlstring "http://ww.noetbook.telent.net/")))
+    (export-server (make-instance 'server :name "ww.noetbook.telent.net" :base-url base-url :port 8000))
+    (export-handler (merge-url base-url "/cliki/")
+		    '(cliki-handler #p"/var/www/cliki/")
+		    :needs-discriminator t)
+    (with-open-file (conf "/tmp/cliki.cf"
+			  :direction :output
+			  :if-does-not-exist :create)
+	(output-apache-conf conf))
+    (install-serve-event-handlers)))
+
 
