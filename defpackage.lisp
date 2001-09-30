1c35c8400de589cc870b8bd9d96b130f4c279406
dan <none>
2001-09-30 23:34:54 +0000

    export a couple of symbols
    darcs-hash:3ada0abd28f3eb3230b93a441db8bd8f6460eda9

--- defpackage.lisp
+++ defpackage.lisp
@@ -1,6 +1,7 @@
 (in-package :cl-user)
 (defpackage "CLIKI"
-  (:export cliki-view-handler cliki-edit-handler)
+  (:export cliki-view-handler cliki-edit-handler cliki-instance cliki-url-root
+	   cliki-css-text cliki-page-header)
   (:use "MAKE" "NET.TELENT.DATE" "COMMON-LISP" "ARANEIDA" "SOCKETS"))
 
 ;;; language for inline searches and stuff
