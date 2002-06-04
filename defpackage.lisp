110abc11446ee95c5d66d7bf20a808764c3df5e1
dan <none>
2002-06-04 00:44:18 +0000

    don't use the MAKE package.  why would we want to?
    darcs-hash:e1dac4a24109eb16de0834841e8cf460b0c4353d

--- defpackage.lisp
+++ defpackage.lisp
@@ -2,7 +2,7 @@
 (defpackage "CLIKI"
   (:export cliki-view-handler cliki-edit-handler cliki-instance cliki-url-root
 	   cliki-css-text cliki-page-header)
-  (:use "MAKE" "NET.TELENT.DATE" "COMMON-LISP" "ARANEIDA" "SOCKETS"))
+  (:use "NET.TELENT.DATE" "COMMON-LISP" "ARANEIDA" "SOCKETS"))
 
 ;;; language for inline searches and stuff
 (defpackage "CLIKI-PAGES" (:use #| nil |# ))
