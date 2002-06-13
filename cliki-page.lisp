5dc506391245c9df5c438c31770730aa32a08a84
dan <none>
2002-06-13 20:49:00 +0000

    SCAN-STREAM is the amazing new function that is given a stream and
    SCAN-STREAM is the amazing new function that is given a stream and
    a set of short forms, and goes through the stream calling stuff when
    it finds any of them
    
    darcs-hash:9d4652891a7e9b5f7b895a01e6732fcc1a6bcb26

--- cliki-page.lisp
+++ cliki-page.lisp
@@ -14,8 +14,25 @@
 (defmethod page-url ((page cliki-page))
   (merge-url (cliki-url-root (page-cliki page))
 	     (urlstring-escape (page-title page))))
-	     
 
 (defmethod print-object ((page cliki-page) stream)
   (print-unreadable-object (page stream :type t :identity t)
 			   (princ (page-title page) stream)))
+
+;;; it would be good to find a more appropriate file for this one
+(defun scan-stream (chars stream dispatch)
+  (let ((c1 (read-char stream)))
+    (dolist (possible chars)
+      (when (eql c1 (car possible))
+	(if (listp (cadr possible))
+	    (let ((r (scan-stream (cadr possible) stream dispatch)))
+	      (return-from scan-stream (if (stringp (car r)) r (cons c1 r))))
+	    (let ((c2 (peek-char nil stream)))
+	      (return-from scan-stream 
+		(if (eql c2 #\( )
+		    (list
+		     (funcall dispatch (cadr possible) 
+			      (read-matched-parens stream)))
+		    (list c1)))))))
+    (list c1)))
+
