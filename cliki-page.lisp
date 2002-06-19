7f4ab73ea50c86372fe8ef86305a7aade12484c0
dan <none>
2002-06-19 02:31:13 +0000

    +(defmethod page-index ((page cliki-page) index)
     +(defmethod page-index ((page cliki-page) index)
     +(defmethod (setf page-index) (new-value (page cliki-page) index)
    scan-stream now side-effects instead of returning short lists of
    characters, making it much easier on the caller.
    
    darcs-hash:7533e780b7a4aa4220e7726cdf61adaa5153d0ea

--- cliki-page.lisp
+++ cliki-page.lisp
@@ -4,9 +4,15 @@
   (merge-pathnames (page-filename page)
 		   (cliki-data-directory (page-cliki page))))
 
+(defmethod page-index ((page cliki-page) index)
+  (cdr (assoc index (page-indices page))))
+
+(defmethod (setf page-index) (new-value (page cliki-page) index)
+  (setf (cdr (assoc index (page-indices page))) new-value))
+
 (defmethod page-tfidf ((page cliki-page))
   (let ((cliki (page-cliki page)))
-    (loop for (term . frequency) in (page-tf page)
+    (loop for (term . frequency) in (page-index page :tf)
 	  collect (cons term
 			(* frequency (log (/ (cliki-number-of-documents cliki)
 					     (cliki-idf cliki term))))))))
@@ -19,20 +25,26 @@
   (print-unreadable-object (page stream :type t :identity t)
 			   (princ (page-title page) stream)))
 
-;;; it would be good to find a more appropriate file for this one
-(defun scan-stream (chars stream dispatch)
-  (let ((c1 (read-char stream)))
-    (dolist (possible chars)
-      (when (eql c1 (car possible))
-	(if (listp (cadr possible))
-	    (let ((r (scan-stream (cadr possible) stream dispatch)))
-	      (return-from scan-stream (if (stringp (car r)) r (cons c1 r))))
-	    (let ((c2 (peek-char nil stream)))
-	      (return-from scan-stream 
-		(if (eql c2 #\( )
-		    (list
-		     (funcall dispatch (cadr possible) 
-			      (read-matched-parens stream)))
-		    (list c1)))))))
-    (list c1)))
+(defun scan-stream (chars stream output dispatch)
+  (labels ((find-token (chars stream dispatch)
+	     (let* ((c1 (read-char stream)))
+	       (cons c1
+		     (dolist (possible chars)
+		       (when (eql c1 (car possible))
+			 (return
+			   (if (listp (cadr possible))
+			       (find-token
+				(cadr possible) stream dispatch)
+			       (let ((c2 (peek-char nil stream)))
+				 (when (eql c2 #\( )
+				   (funcall dispatch (cadr possible) 
+					    (read-matched-parens stream))
+				   (throw 'matched nil)))))))))))
+    (handler-case
+	(loop
+	 (catch 'matched
+	   (dolist (i (find-token chars stream dispatch)) (funcall output i))))
+      (end-of-file (c) (declare (ignore c)) nil))))))))
+
+
 
