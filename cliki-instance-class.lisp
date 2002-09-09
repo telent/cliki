8c6d9c277ddf4d7a9b14e1758b2a776ccfa0d9a1
dan <none>
2002-09-09 23:30:34 +0000

    -(defclass cliki-instance ()
     -(defclass cliki-instance ()
     +(defclass cliki-instance (araneida::dispatching-handler)
    cliki now uses the new araneida dispatch model
    
    darcs-hash:e1f73363e31188a7a5fdc903f3316c83640970c8

--- cliki-instance-class.lisp
+++ cliki-instance-class.lisp
@@ -1,14 +1,14 @@
 (in-package :cliki)
 
-(defclass cliki-instance ()
+(defclass cliki-instance (araneida::dispatching-handler)
   ;; caller should initialize data-directory and url-root; the rest will
   ;; be sorted out internally
   ((data-directory :accessor cliki-data-directory :initarg :data-directory)
    (title :reader cliki-title :initarg :title :initform "CLiki")
+   ;; XXX is this still needed?
    (url-root :accessor cliki-url-root :initarg :url-root)
    (recent-changes :accessor cliki-recent-changes :initform (list))
    (idf)
-   (handlers  :accessor cliki-handlers :initform (list nil))
    (pages :accessor cliki-pages :initform (make-hash-table :test 'equal))
    (short-forms :accessor cliki-short-forms
 		:initarg :short-forms
