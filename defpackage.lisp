a9323ce0349ce409d7618c549946960fbc491bd0
dan <none>
2002-09-09 23:30:39 +0000

    add exports ad-hoc as we find stuff that entomotomy needs
    darcs-hash:2580f7226e5bf7fa90c6b47dba3538757434af1e

--- defpackage.lisp
+++ defpackage.lisp
@@ -1,7 +1,12 @@
 (in-package :cl-user)
 (defpackage "CLIKI"
   (:export cliki-view-handler cliki-edit-handler cliki-instance cliki-url-root
-	   cliki-css-text cliki-page-header)
+	   cliki-css-text cliki-page-header cliki-user-name
+	   request-cliki cliki-data-directory
+	   html-for-keyword
+	   compute-index-for-page add-to-index-for-page make-index-for-page
+	   search-term-relevance
+	   edit-handler)
   (:use "NET.TELENT.DATE" "COMMON-LISP" "ARANEIDA" "SOCKETS"))
 
 ;;; language for inline searches and stuff
