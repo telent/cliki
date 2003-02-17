331d1748fee0b648bcd4a59f2f6288bb1e6c367b
dan <none>
2003-02-17 21:41:58 +0000

    new exports
    darcs-hash:9af0acf136915647aef86d1833a2bc87308019e6

--- defpackage.lisp
+++ defpackage.lisp
@@ -3,14 +3,19 @@
   (:export cliki-view-handler cliki-edit-handler cliki-instance cliki-url-root
 	   cliki-css-text cliki-page-header cliki-page-footer cliki-user-name
 	   write-a-href request-cliki cliki-data-directory
-	   html-for-keyword request-cliki cliki-default-page-name
-	   find-page page-pathname 
+	   html-for-keyword form-element-for-keyword
+	   parse-form-element-for-keyword
+	   request-cliki cliki-default-page-name
+	   cliki-page find-page page-pathname
 	   compute-index-for-page add-to-index-for-page make-index-for-page
 	   search-term-relevance search-term-summary
-	   edit-handler)
+	   edit-handler
+	   
+	   elided-stream)
+	   
   (:import-from #+sbcl "SB-GRAY" #+cmu "EXT"
                 "FUNDAMENTAL-CHARACTER-OUTPUT-STREAM")
-  (:use "NET.TELENT.DATE" "COMMON-LISP" "ARANEIDA" "SOCKETS"))
+  (:use "NET.TELENT.DATE" "COMMON-LISP" "SB-GRAY" "ARANEIDA" "SOCKETS"))
 
 ;;; language for inline searches and stuff
 (defpackage "CLIKI-PAGES" (:use #| nil |# ))
