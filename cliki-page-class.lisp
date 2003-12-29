aaef189669451716adc549351abbcaca7d991253
dan <none>
2003-12-29 23:11:19 +0000

    add slot for versions.  page-pathname now takes keyword arg for version
    add slot for versions.  page-pathname now takes keyword arg for version
    so can no longer be an accessor
    
    darcs-hash:6f806e5c16d1d9f55f50ce711478b48ca9999f92

--- cliki-page-class.lisp
+++ cliki-page-class.lisp
@@ -7,10 +7,11 @@
   ((title :accessor page-title :initarg :title)
    (names :accessor page-names :initarg :names)
    (cliki :accessor page-cliki :initarg :cliki)
+   (versions :accessor page-versions :initarg :versions)
    (indices :accessor page-indices :initarg :indices :initform nil)
    (first-sentence :accessor page-first-sentence :initarg :sentence 
 		   :initform nil)
-   (pathname :accessor page-pathname :initarg :pathname)
+   (pathname :initarg :pathname)	;note no accessor
    (backlinks :accessor page-backlinks :initform nil)
    (topics :accessor page-topics :initform nil)
    ))
