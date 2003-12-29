95982278274b9a4bfb4665e81e57dc49b518ea77
dan <none>
2003-12-29 23:11:35 +0000

    add a :version arg
    darcs-hash:9fe04f200b9cc2c02e95052eb62d65865a31ab3f

--- view-source.lisp
+++ view-source.lisp
@@ -1,11 +1,14 @@
 (in-package :cliki)
 
-(defun view-page-source (request page title)
+(defun view-page-source (request page title &key (version :newest))
   (let ((out (request-stream request)))
     (handler-case
-     (with-open-file (in (page-pathname page) :direction :input)
+     (with-open-file (in (page-pathname page :version version)
+			 :direction :input)
        (request-send-headers request
                              :content-type "text/plain"
+			     :conditional t
+			     :last-modified (file-write-date in)
                              :expires (get-universal-time))
        (araneida::copy-stream in out)
        t)
