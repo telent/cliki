f959c63e8febae5792684ae3eae90bb067793c3e
dan <none>
2004-05-24 21:24:09 +0000

    aif
    darcs-hash:cb6e2031836f769af6b72566e2f6a040b576bd29

--- utilities.lisp
+++ utilities.lisp
@@ -1,5 +1,8 @@
 (in-package :cliki)
 
+(defmacro aif (test then else)
+  `(let ((it ,test)) (if it ,then ,else)))
+
 (defun integer-for (thing &key (default 0) (start 0) end)
   (or (parse-integer (or thing "") :start start :end end :junk-allowed t) 
       default))
